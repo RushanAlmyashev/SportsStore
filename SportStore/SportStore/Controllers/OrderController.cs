38fcaa360c37a48c627b7607ea2699989d396df2
RushanAlmyashev <almyashev91@gmail.com>
2017-12-23 19:13:08 +0300

    Chapter 11

---
+++ SportStore/SportStore/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using System.Linq;
+
+
+namespace SportStore.Controllers
+{
+    public class OrderController :Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repo, Cart cartService)
+        {
+            repository = repo;
+            cart = cartService;
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
+    }
+}
--- SportStore/SportStore/Startup.cs
+++ SportStore/SportStore/Startup.cs
@@ -24,6 +24,7 @@ namespace SportStore
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportsStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
